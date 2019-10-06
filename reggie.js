070d2b7f8bacfe03d2d8c89f5eb68594a6c86baa
i-sayer <i-sayer@users.noreply.github.com>
2019-10-06 14:22:01 +1300

    add tab select function

--- reggie.js
+++ reggie.js
@@ -1,10 +1,11 @@
-function customReg(name, bits, construct){
+function customReg(name, bits, construct, changehandler){
     // move this to js file, maybe pass modules required as attributes?
     customElements.define(name,
         class extends HTMLElement {
             constructor(){
                 super();
                 construct.call(this,bits);
+            }
         }
-    });
+    );
 }
