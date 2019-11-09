7a42e9f91a32d7b28d4b9b487b403cd42b11b466
i-sayer <i-sayer@users.noreply.github.com>
2019-11-10 10:14:56 +1300

    modules in a zip

--- reggie.js
+++ reggie.js
@@ -1,10 +1,13 @@
-function customReg(name, bits, construct, changehandler){
+function customReg(name, bits, construct, connectedhandler){
     // parent helper for components in iframe, defines element using iframe templates constructor callback
     customElements.define(name,
         class extends HTMLElement {
             constructor(){
                 super();
                 construct.call(this,bits);
+            }
+            connectedCallback(){
+                connectedhandler.call(this);
             }
         }
     );
